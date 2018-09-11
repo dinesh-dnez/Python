4fcb042f53f404ebbc2b229a2a76e9840d5a78f6
dinesh-dnez <41505418+dinesh-dnez@users.noreply.github.com>
2018-09-11 16:52:59 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,15 @@
+a = 10
+b = 5
+if a > b :
+	print("a is greater")
+else :
+        print("b is greater")
+        
+print ("Enter your name :")
+name = input()
+if name == 'dinesh' :
+        print ("Hi"+" "+name)
+elif name == "" :
+        print ("please enter your name")
+else :
+        print ("invalid name")
