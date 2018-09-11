d6802b4f10d40837fbd5e01e5faf893b16713453
dinesh-dnez <41505418+dinesh-dnez@users.noreply.github.com>
2018-09-11 16:59:29 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,27 @@
+a = 10
+b = 5
+if a > b :
+	print("a is greater")
+else :
+        print("b is greater")
+        
+print ("Enter your name :")
+name = input()
+if name == 'dinesh' :
+        print ("Hi"+" "+name)
+elif name == "" :
+        print ("please enter your name")
+else :
+        print ("invalid name")
+
+print ("Enter your name")
+name=input()
+print ("Enter your age")
+age=input()
+print ("Hi "+name+" you're "+age+" years old" )
+if int(age)<18:
+    print ("you're not eligible for voting")
+    diff=int(age)-18
+    print ("you can vote after "+diff+" years")
+else:
+    print ("you're eligible for voting")
