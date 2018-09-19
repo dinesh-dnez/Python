317008c96d58721b63b7620ffa75eab1569a6514
dinesh-dnez <41505418+dinesh-dnez@users.noreply.github.com>
2018-09-19 17:18:34 +0530

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,10 @@
+list1 =['Hello',"D",18]
+print ("list1[0]:",list1 [0])
+print ("list1[1]:",list1 [1])
+print ("list1[2]:",list1 [2])
+
+tub = ('Hello',"D",18)
+print (tub)
+
+dict1 = {'Name ':'dinesh','Age ':18}
+print ("Name:",dict1['Name '],"   ","Age :",dict1['Age '])
---
+++ for_loop.py
@@ -0,0 +1,5 @@
+list1 = ["dinesh","daniel","prakash","santhosh","shankar","vasanthi"]
+
+for i in range(6) :
+    for j in list1[i]:
+        print("name",i,":",list1[i])
